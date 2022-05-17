0714e529d552474f6ebedd875d678303642e67c1
mauroAlvarado994 <mauro.alvarado994@gmail.com>
2022-05-17 12:27:14 -0500

    ready form whit js

--- javascript/colaboraciones.js
+++ javascript/colaboraciones.js
@@ -1,8 +1,19 @@
 const $form = document.querySelector('#form')
 
-$form.addEventListener('submit', )
+$form.addEventListener('submit', handleSubmit)
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault()
-    new FormData()
+    const form = new FormData(this)
+    const response =  await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept' : 'aplication/json'
+        }
+    })
+    if (response.ok){
+        this.reset()
+     alert('Gracias por contactarnos!, te devolveremos el mensaje pronto!')
+    }  
 }
