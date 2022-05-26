a820f68daee6a1126e92b828148ab2162f703dfb
mauroAlvarado994 <mauro.alvarado994@gmail.com>
2022-05-25 20:10:34 -0500

    menu mobile redy, justy need to add trancition in the menu

--- javascript/hambuergerMenu.js
+++ javascript/hambuergerMenu.js
@@ -3,9 +3,12 @@ document.querySelector(".hambuergerMenu").addEventListener("click", animateBars)
 var line1_bars = document.querySelector(".line1");
 var line2_bars = document.querySelector(".line2");
 var line3_bars = document.querySelector(".line3");
+// aqui empieza el expermiento
+var menu = document.querySelector(".buttons")
 
 function animateBars(){
     line1_bars.classList.toggle("activeline1");
     line2_bars.classList.toggle("activeline2");
     line3_bars.classList.toggle("activeline3");
+    menu.classList.toggle("menuActive");
 }
